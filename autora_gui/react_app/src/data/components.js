de8a84d094a454801e30442686ab9f7d85eb7bb7
Vladimir Kluzner <vkluzner@jhu.edu>
2026-07-03 19:35:39 -0700

    Update pipInstall parameters for protocols

--- autora_gui/react_app/src/data/components.js
+++ autora_gui/react_app/src/data/components.js
@@ -115,7 +115,7 @@ export const EMBEDDED_COMPONENTS = {
       "githubCommit": "https://github.com/AutoResearch/autora-experimentalist-falsification/blob/main/src/autora/experimentalist/falsification/__init__.py#L22",
       "pythonName": "pool",
       "importPath": "autora.experimentalist.falsification",
-      "pipInstall": "autora-experimentalist-falsification==1.0.0",
+      "pipInstall": "autora-experimentalist-falsification==2.2.0",
       "parameters": {
         "falsification_pooler": [
           {
@@ -292,7 +292,7 @@ export const EMBEDDED_COMPONENTS = {
       "githubCommit": "https://github.com/AutoResearch/autora-experimentalist-falsification/blob/main/src/autora/experimentalist/falsification/__init__.py#L194",
       "pythonName": "sample",
       "importPath": "autora.experimentalist.falsification",
-      "pipInstall": "autora-experimentalist-falsification==1.0.0",
+      "pipInstall": "autora-experimentalist-falsification==2.2.0",
       "parameters": {
         "falsification_sample": [
           {
@@ -431,7 +431,7 @@ export const EMBEDDED_COMPONENTS = {
       "githubCommit": "https://github.com/AutoResearch/autora-core/blob/main/src/autora/experimentalist/grid.py#L9",
       "pythonName": "pool",
       "importPath": "autora.experimentalist.grid",
-      "pipInstall": "autora-experimentalist-grid==1.0.0",
+      "pipInstall": "autora-core==5.0.3",
       "parameters": {
         "grid_pool": []
       },
@@ -469,7 +469,7 @@ export const EMBEDDED_COMPONENTS = {
       "githubCommit": "https://github.com/AutoResearch/autora-experimentalist-lhs/blob/main/src/autora/experimentalist/lhs/__init__.py#L16",
       "pythonName": "pool",
       "importPath": "autora.experimentalist.lhs",
-      "pipInstall": "autora-experimentalist-lhs==1.0.0",
+      "pipInstall": "autora==4.2.0",
       "parameters": {
         "pool": [
           {
@@ -532,7 +532,7 @@ export const EMBEDDED_COMPONENTS = {
       "githubCommit": "https://github.com/AutoResearch/autora-experimentalist-lhs/blob/main/src/autora/experimentalist/lhs/__init__.py#L107",
       "pythonName": "sample",
       "importPath": "autora.experimentalist.lhs",
-      "pipInstall": "autora-experimentalist-lhs==1.0.0",
+      "pipInstall": "autora==4.2.0",
       "parameters": {
         "sample": [
           {
@@ -623,7 +623,7 @@ export const EMBEDDED_COMPONENTS = {
       "githubCommit": "https://github.com/AutoResearch/autora-experimentalist-leverage/blob/main/src/autora/experimentalist/leverage/__init__.py#L8",
       "pythonName": "sample",
       "importPath": "autora.experimentalist.leverage",
-      "pipInstall": "autora-experimentalist-leverage==1.0.0",
+      "pipInstall": "autora-experimentalist-leverage==1.10",
       "parameters": {
         "leverage_sample": [
           {
@@ -742,7 +742,7 @@ export const EMBEDDED_COMPONENTS = {
       "githubCommit": "https://github.com/blinodelka/mixture_experimental_strategies/blob/main/src/autora/experimentalist/mixture/__init__.py#L24",
       "pythonName": "sample",
       "importPath": "autora.experimentalist.mixture",
-      "pipInstall": "experimentalist-sampler-mixture==1.0.0",
+      "pipInstall": "mixture-experimentalist==1.0.0a7",
       "parameters": {
         "mixture_sample": [
           {
@@ -837,7 +837,7 @@ export const EMBEDDED_COMPONENTS = {
       "githubCommit": "https://github.com/AutoResearch/autora-experimentalist-model-disagreement/blob/main/src/autora/experimentalist/model_disagreement/__init__.py#L287",
       "pythonName": "sample",
       "importPath": "autora.experimentalist.model_disagreement",
-      "pipInstall": "autora-experimentalist-model-disagreement==1.0.0",
+      "pipInstall": "autora-experimentalist-model-disagreement==2.2.0",
       "parameters": {
         "model_disagreement_sampler": [
           {
@@ -904,7 +904,7 @@ export const EMBEDDED_COMPONENTS = {
       "githubCommit": "https://github.com/AutoResearch/autora-experimentalist-nearest-value/blob/main/src/autora/experimentalist/nearest_value/__init__.py#L9",
       "pythonName": "sample",
       "importPath": "autora.experimentalist.nearest_value",
-      "pipInstall": "autora-experimentalist-nearest_value==1.0.0",
+      "pipInstall": "autora-experimentalist-nearest-value==2.2.0",
       "parameters": {
         "nearest_values_sampler": [
           {
@@ -971,7 +971,7 @@ export const EMBEDDED_COMPONENTS = {
       "githubCommit": "https://github.com/AutoResearch/autora-experimentalist-novelty/blob/main/src/autora/experimentalist/novelty/__init__.py#L37",
       "pythonName": "sample",
       "importPath": "autora.experimentalist.novelty",
-      "pipInstall": "autora-experimentalist-novelty==1.0.0",
+      "pipInstall": "autora-experimentalist-novelty==2.2.0",
       "parameters": {
         "novelty_sampler": [
           {
@@ -1075,7 +1075,7 @@ export const EMBEDDED_COMPONENTS = {
       "githubCommit": "https://github.com/AutoResearch/autora-experimentalist-novelty/blob/main/src/autora/experimentalist/novelty/__init__.py#L81",
       "pythonName": "score_sample",
       "importPath": "autora.experimentalist.novelty",
-      "pipInstall": "autora-experimentalist-novelty==1.0.0",
+      "pipInstall": "autora-experimentalist-novelty==2.2.0",
       "parameters": {
         "novelty_score_sampler": [
           {
@@ -1179,7 +1179,7 @@ export const EMBEDDED_COMPONENTS = {
       "githubCommit": "https://github.com/AutoResearch/autora-experimentalist-prediction-filter/blob/main/src/autora/experimentalist/prediction_filter/__init__.py#L11",
       "pythonName": "filter",
       "importPath": "autora.experimentalist.prediction_filter",
-      "pipInstall": "autora-experimentalist-prediction-filter==1.0.0",
+      "pipInstall": "autora-experimentalist-prediction-filter==1.1.0",
       "parameters": {
         "prediction_filter": [
           {
@@ -1246,7 +1246,7 @@ export const EMBEDDED_COMPONENTS = {
       "githubCommit": "https://github.com/AutoResearch/autora-core/blob/main/src/autora/experimentalist/random.py#L9",
       "pythonName": "pool",
       "importPath": "autora.experimentalist.random",
-      "pipInstall": "autora-experimentalist-random==1.0.0",
+      "pipInstall": "autora-core==5.0.3",
       "parameters": {
         "random_pool": [
           {
@@ -1321,7 +1321,7 @@ export const EMBEDDED_COMPONENTS = {
       "githubCommit": "https://github.com/AutoResearch/autora-core/blob/main/src/autora/experimentalist/random.py#L137",
       "pythonName": "sample",
       "importPath": "autora.experimentalist.random",
-      "pipInstall": "autora-experimentalist-random==1.0.0",
+      "pipInstall": "autora-core==5.0.3",
       "parameters": {
         "random_sample": [
           {
@@ -1396,7 +1396,7 @@ export const EMBEDDED_COMPONENTS = {
       "githubCommit": "https://github.com/AutoResearch/autora-experimentalist-inequality/blob/main/src/autora/experimentalist/inequality/__init__.py#L33",
       "pythonName": "sample",
       "importPath": "autora.experimentalist.inequality",
-      "pipInstall": "autora-experimentalist-inequality==1.0.0",
+      "pipInstall": "autora-experimentalist-inequality==2.2.0",
       "parameters": {
         "summed_inequality_sampler": [
           {
@@ -1497,7 +1497,7 @@ export const EMBEDDED_COMPONENTS = {
       "githubCommit": "https://github.com/AutoResearch/autora-experimentalist-uncertainty/blob/main/src/autora/experimentalist/uncertainty/__init__.py#L10",
       "pythonName": "sample",
       "importPath": "autora.experimentalist.uncertainty",
-      "pipInstall": "autora-experimentalist-uncertainty==1.0.0",
+      "pipInstall": "autora-experimentalist-uncertainty==2.1.0",
       "parameters": {
         "uncertainty_sampler": [
           {
@@ -1582,7 +1582,7 @@ export const EMBEDDED_COMPONENTS = {
       "githubCommit": "https://github.com/AutoResearch/autora-theorist-bms/blob/ce3d7edf707c54a12108700076d841c138086be1/src/autora/theorist/bms/regressor.py#L24",
       "pythonName": "BMSRegressor",
       "importPath": "autora.theorist.bms.regressor",
-      "pipInstall": "autora-theorist-bms==1.0.0",
+      "pipInstall": "autora-theorist-bms==1.0.6",
       "parameters": {
         "__init__": [
           {
@@ -2151,7 +2151,7 @@ export const EMBEDDED_COMPONENTS = {
       "githubCommit": "https://github.com/AutoResearch/autora-experiment-runner-firebase-prolific/blob/main/src/autora/experiment_runner/firebase_prolific/__init__.py#L267",
       "pythonName": "firebase_prolific_runner",
       "importPath": "autora.experiment_runner.firebase_prolific",
-      "pipInstall": "autora-experiment-runner-firebase-prolific==1.0.0",
+      "pipInstall": "autora-experiment-runner-firebase-prolific==1.0.1",
       "parameters": {
         "firebase_prolific_runner": [
           {
@@ -2322,7 +2322,7 @@ export const EMBEDDED_COMPONENTS = {
       "githubCommit": "https://github.com/AutoResearch/autora-experiment-runner-firebase-prolific/blob/main/src/autora/experiment_runner/firebase_prolific/__init__.py#L291",
       "pythonName": "firebase_runner",
       "importPath": "autora.experiment_runner.firebase_prolific",
-      "pipInstall": "autora-experiment-runner-firebase-prolific==1.0.0",
+      "pipInstall": "autora-experiment-runner-firebase-prolific==1.0.1",
       "parameters": {
         "firebase_runner": [
           {
@@ -2405,7 +2405,7 @@ export const EMBEDDED_COMPONENTS = {
       "githubCommit": "https://github.com/AutoResearch/autora-synthetic-abstract-equation/blob/main/src/autora/experiment_runner/synthetic/abstract/equation/__init__.py#L14",
       "pythonName": "equation_experiment",
       "importPath": "autora.experiment_runner.synthetic.abstract.equation",
-      "pipInstall": "autora-synthetic-abstract-equation==1.0.0",
+      "pipInstall": "autora-synthetic==2.2.0",
       "parameters": {
         "equation_experiment": [
           {
@@ -2510,7 +2510,7 @@ export const EMBEDDED_COMPONENTS = {
       "githubCommit": "https://github.com/AutoResearch/autora-synthetic/blob/main/src/autora/experiment_runner/synthetic/abstract/lmm.py#L164",
       "pythonName": "lmm_experiment",
       "importPath": "autora.experiment_runner.synthetic.abstract.lmm",
-      "pipInstall": "autora-synthetic==1.0.0",
+      "pipInstall": "autora-synthetic==2.2.0",
       "parameters": {
         "lmm_experiment": [
           {
@@ -2601,7 +2601,7 @@ export const EMBEDDED_COMPONENTS = {
       "githubCommit": "https://github.com/AutoResearch/autora-synthetic/blob/main/src/autora/experiment_runner/synthetic/economics/expected_value_theory.py#L71",
       "pythonName": "expected_value_theory",
       "importPath": "autora.experiment_runner.synthetic.economics.expected_value_theory",
-      "pipInstall": "autora-synthetic==1.0.0",
+      "pipInstall": "autora-synthetic==2.2.0",
       "parameters": {
         "expected_value_theory": [
           {
@@ -2766,7 +2766,7 @@ export const EMBEDDED_COMPONENTS = {
       "githubCommit": "https://github.com/AutoResearch/autora-synthetic/blob/main/src/autora/experiment_runner/synthetic/economics/prospect_theory.py#L13",
       "pythonName": "prospect_theory",
       "importPath": "autora.experiment_runner.synthetic.economics.prospect_theory",
-      "pipInstall": "autora-synthetic==1.0.0",
+      "pipInstall": "autora-synthetic==2.2.0",
       "parameters": {
         "prospect_theory": [
           {
@@ -2979,7 +2979,7 @@ export const EMBEDDED_COMPONENTS = {
       "githubCommit": "https://github.com/AutoResearch/autora-synthetic/blob/main/src/autora/experiment_runner/synthetic/neuroscience/task_switching.py#L11",
       "pythonName": "task_switching",
       "importPath": "autora.experiment_runner.synthetic.neuroscience.task_switching",
-      "pipInstall": "autora-synthetic==1.0.0",
+      "pipInstall": "autora-synthetic==2.2.0",
       "parameters": {
         "task_switching": [
           {
@@ -3135,7 +3135,7 @@ export const EMBEDDED_COMPONENTS = {
       "githubCommit": "https://github.com/AutoResearch/autora-synthetic/blob/main/src/autora/experiment_runner/synthetic/psychology/exp_learning.py#L11",
       "pythonName": "exp_learning",
       "importPath": "autora.experiment_runner.synthetic.psychology.exp_learning",
-      "pipInstall": "autora-synthetic==1.0.0",
+      "pipInstall": "autora-synthetic==2.2.0",
       "parameters": {
         "exp_learning": [
           {
@@ -3288,7 +3288,7 @@ export const EMBEDDED_COMPONENTS = {
       "githubCommit": "https://github.com/AutoResearch/autora-synthetic/blob/main/src/autora/experiment_runner/synthetic/psychology/luce_choice_ratio.py#L11",
       "pythonName": "luce_choice_ratio",
       "importPath": "autora.experiment_runner.synthetic.psychology.luce_choice_ratio",
-      "pipInstall": "autora-synthetic==1.0.0",
+      "pipInstall": "autora-synthetic==2.2.0",
       "parameters": {
         "luce_choice_ratio": [
           {
@@ -3429,7 +3429,7 @@ export const EMBEDDED_COMPONENTS = {
       "githubCommit": "https://github.com/AutoResearch/autora-synthetic/blob/main/src/autora/experiment_runner/synthetic/psychology/q_learning.py#L114",
       "pythonName": "q_learning",
       "importPath": "autora.experiment_runner.synthetic.psychology.q_learning",
-      "pipInstall": "autora-synthetic==1.0.0",
+      "pipInstall": "autora-synthetic==2.2.0",
       "parameters": {
         "q_learning": [
           {
@@ -3570,7 +3570,7 @@ export const EMBEDDED_COMPONENTS = {
       "githubCommit": "https://github.com/AutoResearch/autora-synthetic/blob/main/src/autora/experiment_runner/synthetic/psychophysics/stevens_power_law.py#L11",
       "pythonName": "stevens_power_law",
       "importPath": "autora.experiment_runner.synthetic.psychophysics.stevens_power_law",
-      "pipInstall": "autora-synthetic==1.0.0",
+      "pipInstall": "autora-synthetic==2.2.0",
       "parameters": {
         "stevens_power_law": [
           {
@@ -3683,7 +3683,7 @@ export const EMBEDDED_COMPONENTS = {
       "githubCommit": "https://github.com/AutoResearch/autora-synthetic/blob/main/src/autora/experiment_runner/synthetic/psychophysics/weber_fechner_law.py#L11",
       "pythonName": "weber_fechner_law",
       "importPath": "autora.experiment_runner.synthetic.psychophysics.weber_fechner_law",
-      "pipInstall": "autora-synthetic==1.0.0",
+      "pipInstall": "autora-synthetic==2.2.0",
       "parameters": {
         "weber_fechner_law": [
           {
